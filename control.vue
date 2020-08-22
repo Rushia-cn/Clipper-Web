b75cdd99181844995096ca7670e7303f5b9503c7
Pop <gm@georgemiao.com>
2020-08-22 11:48:37 +0800

    ✨feat: add loop function

--- control.vue
+++ control.vue
@@ -9,7 +9,11 @@
             <button type="button" @click="jump(-3)"> -3s </button>
             <button type="button" @click="jump(-1)"> -1s </button>
             <input v-model="inpTime" @keydown.enter="jumpTo(inpTime)">
-            {{curTime}}
+            {{formatTime(curTime)}}
+            <div v-if="looping">
+                Looping [{{formatTime(loopTS[0])}} - {{formatTime(loopTS[1])}}]
+                <button type="button" @click="clearLoop">Clear Loop</button>
+            </div>
             <button type="button" @click="jump(1)"> +1s </button>
             <button type="button" @click="jump(3)"> +3s </button>
         </div>
@@ -18,8 +22,9 @@
             <button type="button" @click="addTo">To</button>
         </div>
         <div id="timestamp-group" v-for="(ts, i) in timeStamps" :key="i">
-            <button type="button" @click="jumpTo(ts.from)"> {{ (ts.from !== null) ? ts.from : "N/A" }} </button>
-            <button type="button" @click="jumpTo(ts.to)"> {{ (ts.to !== null) ? ts.to : "N/A" }} </button>
+            <button type="button" @click="jumpTo(ts.from)"> {{ formatTime(ts.from) }} </button>
+            <button type="button" @click="jumpTo(ts.to)"> {{ formatTime(ts.to) }} </button>
+            <button type="button" @click="loop(ts.from, ts.to)">Loop</button>
             <button type="button" @click="timeStamps.pop(i)">X</button>
         </div>
         <div id="metaInput">
@@ -48,7 +53,9 @@ export default {
             playerVar:{ rel: 0 },
             inpTime: 0,
             curTime: 0,
-            timeStamps: []
+            timeStamps: [],
+            looping: false,
+            loopTS: []
         }
     },
     computed: {
@@ -62,6 +69,30 @@ export default {
         },
     },
     methods:{
+        loop(from, to){
+            if (!from || !to) {
+                alert("Complete the timelapese")
+                return
+            }
+            this.looping = true
+            this.loopTS[0] = this.parseTime(from)
+            this.loopTS[1] = this.parseTime(to)
+            this.jump(this.jumpTo[0])
+        },
+        clearLoop(){
+            this.looping = false
+            this.loopTS = []
+        },
+        formatTime(v){
+            if (v === null) return "N/A"
+            v = v.toString()
+            if (v === "0") return 0
+            const splited = v.split(".")
+            const date = new Date(0)
+            date.setSeconds(Number(splited[0]))
+            date.setMilliseconds(Number(splited[1]))
+            return date.toISOString().substr(11, 12)
+        },
         videoReady(event){
             this.ytb = event.target
             this.updateCurrentTime()
@@ -74,9 +105,10 @@ export default {
             this.ytb.seekTo(targetTime)
         },
         jumpTo(sec){
-            const parsed = this.parseTime(sec)
-            if (parsed === -1) return
-            this.ytb.seekTo(parsed)
+            if (typeof sec === "string")
+                sec = this.parseTime(sec)
+            if (sec === -1) return
+            this.ytb.seekTo(sec)
         },
         parseTime(str){
             try{
@@ -94,7 +126,11 @@ export default {
         },
         updateCurrentTime() {
             this.curTime = Number(this.ytb.getCurrentTime()).toFixed(3)
-            requestAnimationFrame(this.updateCurrentTime)
+            if (this.curTime >= this.loopTS[1]) this.jumpTo(this.loopTS[0].toString())
+            const that = this
+            setTimeout(function(){
+                requestAnimationFrame(that.updateCurrentTime)
+            }, 50);
         },
         addFrom(){
             if (this.lastComplete)
